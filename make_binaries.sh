e8c65627286d815834b27ddfc3c1d23c3bac4a35
Gogs <gogs@fake.local>
2025-06-08 12:31:33 +0530

    delete orig src

--- make_binaries.sh
+++ make_binaries.sh
@@ -19,6 +19,7 @@ fi
 export VERSION=$(echo $GITHUB_REF_NAME | cut -d'-' -f1)
 wget https://www.kernel.org/pub/software/scm/git/git-${VERSION}.tar.gz
 tar zxf git-${VERSION}.tar.gz
+rm git-${VERSION}.tar.gz
 mv git-${VERSION} git-${VERSION}-src
 pushd git-${VERSION}-src
 ./configure --prefix=$PWD/../git-${VERSION} --with-curl --with-openssl
